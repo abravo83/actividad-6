8151896ce23dfe7284fe76f9bc1d3118bb1de872
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2024-08-07 00:19:47 +0200

    Create component navbar, pages main & home, created iuser interface. Add routes '', 'main' and 'main/home'

---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css',
+})
+export class MainComponent {}
