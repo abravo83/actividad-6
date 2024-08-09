de62a89c110e9fb2fd4b4a4e299b7ab5e538552f
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2024-08-09 14:06:58 +0200

    Added new-user component. Removed unnecesary imports. Filled whole nav-bar component. Added routes for the pages param, newuser and wildcard. Added whole home component content and login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { NewUserComponent } from './pages/new-user/new-user.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'home/:page', component: HomeComponent },
+  { path: 'newuser', component: NewUserComponent },
+  { path: '**', redirectTo: '/home' },
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,102 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Iuser } from '../../interfaces/iuser.interface';
+import { UsersService } from '../../services/users.service';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  // INJECTABLES
+  usersService = inject(UsersService);
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
 
+  // PROPERTIES
+  users: Iuser[] = [];
+  page: number = 1;
+  lastPage: number = 2;
+
+  /**
+   * Inicializa el componente.
+   * Si el parámetro 'page' se encuentra presente, se actualiza el componente con los datos de la pagina solicitada.
+   * Si el parámetro 'page' no se encuentra presente, se actualiza el componente con todos los datos.
+   *
+   * @return {void} Esta función no devuelve ningun valor.
+   */
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      if (params['page']) {
+        this.page = params['page'];
+        this.usersService.getUsers(params['page']).subscribe((users: any) => {
+          this.users = users.results as Iuser[];
+          this.lastPage = users.total_pages;
+        });
+      } else {
+        this.usersService.getUsers().subscribe((users: any) => {
+          this.users = users.results as Iuser[];
+        });
+      }
+    });
+  }
+
+  /**
+   * Una función que comprueba si la URL actual coincide con la URL proporcionada.
+   *
+   * @param {string} url - La URL a comprobar.
+   * @return {boolean} Verdadero si la URL actual coincide con la URL proporcionada, de lo contrario falso.
+   */
+  esRutaActiva(url: string): boolean {
+    return this.router.url === url;
+  }
+
+  /**
+   * Navega a la página siguiente.
+   *
+   * @return {void} Sin valor de retorno.
+   */
+  goToNextPage(): void {
+    if (typeof this.page !== 'undefined') {
+      if (this.page < 2) {
+        this.router.navigate(['/home', this.page + 1]);
+      }
+
+      if (this.page > this.lastPage) {
+        this.page = this.lastPage;
+        this.router.navigate(['/home', this.lastPage]);
+      } else {
+        this.router.navigate(['/home', 2]);
+      }
+    }
+  }
+
+  /**
+   * Navega a la página anterior.
+   *
+   * @return {void} Sin valor de retorno.
+   */
+  goToPreviousPage(): void {
+    if (typeof this.page !== 'undefined') {
+      if (this.page > 1) {
+        this.router.navigate(['/home', this.page - 1]);
+      }
+
+      if (this.page < 1) {
+        this.page = 1;
+        this.router.navigate(['/home', 1]);
+      }
+    } else {
+      this.router.navigate(['/home', 1]);
+    }
+  }
 }
