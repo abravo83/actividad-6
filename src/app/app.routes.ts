8be1e008794ef4dd02d6c891f0085782136a1b02
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2024-08-09 14:13:56 +0200

    Added routes for the user component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NewUserComponent } from './pages/new-user/new-user.component';
+import { UserComponent } from './pages/user/user.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'home/:page', component: HomeComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'user/:_id', component: UserComponent },
   { path: 'newuser', component: NewUserComponent },
   { path: '**', redirectTo: '/home' },
 ];
