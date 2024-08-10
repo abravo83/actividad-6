ec940fdd14b126c269ae329845c397ee973e3780
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2024-08-10 07:41:24 +0200

    Added updateuser component to pages. Added route to it with param _id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NewUserComponent } from './pages/new-user/new-user.component';
 import { UserComponent } from './pages/user/user.component';
+import { UpdateuserComponent } from './pages/updateuser/updateuser.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -9,5 +10,6 @@ export const routes: Routes = [
   { path: 'home/:page', component: HomeComponent },
   { path: 'user/:_id', component: UserComponent },
   { path: 'newuser', component: NewUserComponent },
+  { path: 'updateuser/:_id', component: UpdateuserComponent },
   { path: '**', redirectTo: '/home' },
 ];
