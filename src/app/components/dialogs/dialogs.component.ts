2795e3f84f99130052f92931aae4599dbfed4cd7
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2024-08-13 20:57:04 +0200

    Adding signal and signal functionality to the dialog component

--- src/app/components/dialogs/dialogs.component.ts
+++ src/app/components/dialogs/dialogs.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { DialogsService } from '../../services/dialogs.service';
 
 @Component({
   selector: 'app-dialogs',
   standalone: true,
   imports: [],
   templateUrl: './dialogs.component.html',
-  styleUrl: './dialogs.component.css'
+  styleUrl: './dialogs.component.css',
 })
 export class DialogsComponent {
+  // INJECTABLES
+  dialogsService = inject(DialogsService);
 
+  // PROPERTIES
+  showConfirmationDialog: boolean = false;
+
+  constructor() {
+    effect(() => {
+      this.showConfirmationDialog =
+        this.dialogsService.signalShowConfirmationDialog();
+    });
+  }
 }
